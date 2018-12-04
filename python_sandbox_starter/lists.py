06dec0e674b81af0f7546760fa81d223eab9a8a0
Perez-Silva, Davian S <dsperezsilva171@stevenscollege.edu>
2018-12-03 19:16:09 -0500

    Lists Basics

--- python_sandbox_starter/lists.py
+++ python_sandbox_starter/lists.py
@@ -1,1 +1,59 @@
-# A List is a collection which is ordered and changeable. Allows duplicate members.
+# A List is a collection which is ordered and changeable. 
+# Allows duplicate members.
+
+# Create list
+
+numbers = [1, 2, 3, 4, 5]
+fruits = ['Apples', 'Oranges', 'Grapes', 'Pears']
+
+# Use a constructor
+#numbers2 = list((1, 2, 3, 4, 5))
+
+# Get a value
+print(fruits[1])
+
+# Get lenght
+print(len(fruits))
+
+# Append to list. To add
+fruits.append('Mangos')
+
+print(fruits)
+
+# Remove from list
+
+fruits.remove('Grapes')
+
+print(fruits)
+
+# Insert into position
+fruits.insert(2, 'Strawberries')
+
+print(fruits)
+
+# Change value
+fruits[0] = 'Quenepas'
+
+print(fruits)
+
+# Remove with pop
+fruits.pop(2)
+
+print(fruits)
+
+# Reverse list
+fruits.reverse()
+
+print(fruits)
+
+# Sort list in alphabetical order
+fruits.sort()
+
+print(fruits)
+
+# Reverse sort
+
+fruits.sort(reverse=True)
+
+print(fruits)
+
