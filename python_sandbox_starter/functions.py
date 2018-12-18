12bcff44ebf7d4a06b7493785b01676a717dcdde
Perez-Silva, Davian S <dsperezsilva171@stevenscollege.edu>
2018-12-17 19:08:46 -0500

    Conditionals

--- python_sandbox_starter/functions.py
+++ python_sandbox_starter/functions.py
@@ -1,10 +1,10 @@
 # A function is a block of code which only runs when it is called. In Python, we do not use parentheses and curly brackets, we use indentation with tabs or spaces
 
 # Create function
-def sayHello(name='Davian'):
+def sayHello(name):
     print(f'Hello {name}')
 
-sayHello() # or sayHello('Davian') and take out value up there
+sayHello("Davian") 
 
 # Return values
 def getSum(num1, num2):
